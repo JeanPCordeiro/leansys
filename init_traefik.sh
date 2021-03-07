5340ab4c6cf396178fb237aa73fe4aa25a90c257
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2021-03-07 10:30:00 +0100

    init traefik

---
+++ init_traefik.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker network create --driver=overlay traefik-public
+export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
+docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
+export EMAIL=jeanpierre.cordeiro@gmail.com
+export DOMAIN=traefik.mercatum.fr
+export USERNAME=admin
+export HASHED_PASSWORD=$(openssl passwd -apr1)
+curl -L dockerswarm.rocks/traefik-host.yml -o traefik-host.yml
+docker stack deploy -c traefik-host.yml traefik
+docker stack ps traefik
+docker service logs traefik_traefik
