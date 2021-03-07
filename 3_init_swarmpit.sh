75256eaf292fcd321e441b2a9a96eae092aac37b
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2021-03-07 16:02:38 +0100

    final

--- 3_init_swarmpit.sh
+++ 3_init_swarmpit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DOMAIN=swarmpit.mercatum.fr
+export DOMAIN=swarmpit.sys.hostby.link
 export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add swarmpit.db-data=true $NODE_ID
 docker node update --label-add swarmpit.influx-data=true $NODE_ID
