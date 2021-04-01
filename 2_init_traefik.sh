f5aaf9e05ba748e3430b4dd30b2fa2b0e5156d53
root <root@vmi536198.contaboserver.net>
2021-04-01 10:43:02 +0200

    save

--- 2_init_traefik.sh
+++ 2_init_traefik.sh
@@ -2,8 +2,8 @@
 docker network create --driver=overlay traefik-public
 export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
-export EMAIL=jeanpierre.cordeiro@gmail.com
-export DOMAIN=traefik.sys.hostby.link
+export EMAIL=onebuckcloud@gmail.com
+export DOMAIN=traefik.sys.onebuck.cloud
 export USERNAME=admin
 export HASHED_PASSWORD=$(openssl passwd -apr1)
 #curl -L dockerswarm.rocks/traefik-host.yml -o traefik-host.yml
--- 4_init_swarmprom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone https://github.com/stefanprodan/swarmprom.git
-cd swarmprom
-export ADMIN_USER=admin
-export HASHED_PASSWORD=$(openssl passwd -apr1)
-export DOMAIN=sys.hostby.link
-#curl -L dockerswarm.rocks/swarmprom.yml -o swarmprom.yml
-docker stack deploy -c swarmprom.yml swarmprom
-docker stack ps swarmprom
