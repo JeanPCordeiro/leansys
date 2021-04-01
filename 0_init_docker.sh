269feb5c07149e17f17283fa70820580e87786a2
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2021-04-01 16:57:32 +0200

    docker ubuntu

--- 0_init_docker.sh
+++ 0_init_docker.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-apt install sudo
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
-sudo adduser jpierre
-sudo usermod -aG sudo jpierre
-sudo usermod -aG docker jpierre
+
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt update -y
+sudo apt install docker-ce -y
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+
