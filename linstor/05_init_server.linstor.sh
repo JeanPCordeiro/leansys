2fb9e1891e9814e6a35a6fce341514cb325c2882
root <root@vmi536198.contaboserver.net>
2021-05-01 16:38:52 +0200

    linstor ok

--- linstor/05_init_server.linstor.sh
+++ linstor/05_init_server.linstor.sh
@@ -5,15 +5,15 @@ set -o xtrace
 #
 #
 #docker plugin install linbit/linstor-docker-volume --alias linstor --disable
-docker plugin install lade/linstor --alias linstor --disable
+docker plugin install lade/linstor linstor --disable
 cp docker-volume.conf /etc/linstor/docker-volume.conf
-docker plugin enable linstor
+docker plugin enable lade/linstor
 
 #
 # Create test Volume
 #
 SERVER=`hostname -s`
 if [ "$SERVER" = vmi536198  ]; then
-   docker volume create -d linstor --opt fs=ext4 --opt size=2G testvol 
-   docker run -it --rm --name=cont -v testvol:/data --volume-driver=linstor busybox ps aux
+   docker volume create -d lade/linstor --opt size=2G testvol 
+   docker run -it --rm --name=cont -v testvol:/data --volume-driver=lade/linstor busybox ps aux
 fi
