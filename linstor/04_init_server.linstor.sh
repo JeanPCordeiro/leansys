1d6c2737d6cc42cc6f91bcd9ccc61c91596fd25f
root <root@vmi536198.contaboserver.net>
2021-05-03 11:29:34 +0200

    convoy

--- linstor/04_init_server.linstor.sh
+++ linstor/04_init_server.linstor.sh
@@ -6,12 +6,20 @@ set -o xtrace
 #
 SERVER=`hostname -s`
 if [ "$SERVER" = vmi536198  ]; then
+  
   linstor physical-storage create-device-pool --pool-name disk150G LVM vmi536198.contaboserver.net /dev/loop0
   linstor physical-storage create-device-pool --pool-name disk150G LVM vmi522170.contaboserver.net /dev/loop0
   linstor physical-storage create-device-pool --pool-name disk150G LVM vmi576145.contaboserver.net /dev/loop0
+
   linstor storage-pool create lvm vmi536198.contaboserver.net pool_ssd disk150G
   linstor storage-pool create lvm vmi522170.contaboserver.net pool_ssd disk150G
   linstor storage-pool create lvm vmi576145.contaboserver.net pool_ssd disk150G
   linstor sp l
+
+  linstor resource-definition create nfs_data
+  linstor volume-definition create nfs_data 140G
+  linstor resource create nfs_data  --auto-place 3
+  linstor volume list
+
 fi
 
--- linstor/05_init_server.linstor.sh
+++ linstor/05_init_server.linstor.sh
@@ -2,18 +2,45 @@
 set -o xtrace
 
 #
+# Install NFS
 #
+
+apt install -y nfs-kernel-server
+apt install -y nfs-common
+cp exports /etc/exports
+mkdir /nfs_data
+
+SERVER=`hostname -s`
+if [ "$SERVER" = vmi536198  ]; then
+  mkfs.ext4 /dev/drbd1000
+  cat fstab.client >> /etc/fstab
+  systemctl enable nfs-server
+  systemctl start nfs-server
+  mount -a
+  mkdir -p /nfs_data/docker/volumes/config
+fi
+cat fstab.client >> /etc/fstab
+mount -a
+
 #
-#docker plugin install linbit/linstor-docker-volume --alias linstor --disable
-docker plugin install lade/linstor linstor --disable
-cp docker-volume.conf /etc/linstor/docker-volume.conf
-docker plugin enable lade/linstor
+# Install Convoy Plugin
+#
+wget https://github.com/rancher/convoy/releases/download/v0.5.2/convoy.tar.gz
+tar xvzf convoy.tar.gz
+cp convoy/convoy convoy/convoy-pdata_tools /usr/local/bin/
+bash -c 'echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/convoy.spec'
+cp convoy-plugin /etc/init.d/convoy-plugin
+chmod +x /etc/init.d/convoy
+systemctl enable convoy
+systemctl start convoy
+systemctl status convoy
 
 #
 # Create test Volume
 #
 SERVER=`hostname -s`
 if [ "$SERVER" = vmi536198  ]; then
-   docker volume create -d lade/linstor --opt size=10M initvol
+   docker volume create --name initvol --volume-driver=convoy
    docker run -it --rm --name=cont -v initvol:/data --volume-driver=lade/linstor busybox ps aux
 fi
+
